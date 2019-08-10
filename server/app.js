7e66c61f479785bb4b74cd0a99359c390b9a645c
andrew <andrewjthomsen@yahoo.com>
2019-08-10 14:56:42 -0700

    Transfered files over from an old repo that utilized starter code into the current repository. Managed to get all test cases passing

--- server/app.js
+++ server/app.js
@@ -1,20 +1,82 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.use((req, res, next) => {
-// write your logging code here
+  // write your logging code here
+  let agent = req.headers["user-agent"].replace(/,/, "");
+  let time = new Date().toISOString();
+  let method = req.method;
+  let resource = req.path;
+  let version = `HTTP/$(req.httpVersion)`;
+  let status = 200;
 
-});
+  let dataInfo =
+    agent +
+    "," +
+    time +
+    "," +
+    method +
+    "," +
+    resource +
+    "," +
+    version +
+    status +
+    "\n";
+  console.log(dataInfo);
 
-app.get('/', (req, res) => {
-// write your code to respond "ok" here
+  // REsponsible for writing data to csv file
+  fs.appendFile("./log.csv", dataInfo, "utf8", error => {
+    if (error) throw error;
+    // Otherwise, if no error
+    next();
+  });
 
-});
+  app.get("/", (req, res) => {
+    // write your code to respond "ok" here
+    res.send("ok");
+  });
 
-app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+  app.get("/logs", (req, res) => {
+    // write your code to return a json object containing the log data here
+    fs.readFile("./log.csv", "utf8", (error, dataInfo) => {
+      var dataObject = "{";
+      var lines = data.split("\n");
+      var items = undefined;
+      for (var i = 1; i < lines.length; i++) {
+        if (lines[i].length > 2) {
+          items = lines[i].split(",");
+          object +=
+            "{" +
+            '"Agent":"' +
+            items[0] +
+            '",' +
+            '"Time":"' +
+            items[1] +
+            '",' +
+            '"Method":"' +
+            items[2] +
+            '",' +
+            '"Resource":"' +
+            items[3] +
+            '",' +
+            '"Version":"' +
+            items[4] +
+            '",' +
+            '"Status":"' +
+            items[5] +
+            '"' +
+            "}";
 
+          if (i < lines.length - 2) {
+            object += ",";
+          }
+        }
+      }
+      object += "}";
+      res.json(JSON.parse(object));
+    });
+  });
 });
 
 module.exports = app;
